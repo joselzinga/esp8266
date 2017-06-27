4f906255c364276e159445a379ed5331d3cc67f0
joselzinga <github@joselzinga.nl>
2017-06-27 21:55:35 +0200

    Added display

--- esp8266.ino
+++ esp8266.ino
@@ -23,38 +23,38 @@
 
 #include <ESP8266WiFi.h>
 #include "DHT.h"
-#include "settings.h"  
-
+#include <U8x8lib.h>
+#include "settings.h"
 
 /***************************
-   Begin Settings
- **************************/
+  Edit settings.h for appropriate WiFi-settings, update interval, Thingspeak API,
+  Domoticz hostname/Sensor-ID and Weather Underground ID/password.
+**************************/
 
- // Edit settings.h for appropriate WiFi-settings, update interval, Thingspeak API, Domoticz hostname/Sensor-ID and Weather Underground ID/password.
+// We define the type of screen here
+// I use: https://www.tinytronics.nl/shop/nl/display/0.96-inch-oled-display-128*64-pixels-blauw-i2c?search=I2C0.96OLEDBLUE
+// With SCL to D2, SCA to D1 (software I2C protocol)
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(D2, D1, U8X8_PIN_NONE);
 
- 
- 
-/***************************
-   End Settings
- **************************/
-
-// Initialize the temperature/ humidity sensor
+// Initialize the temperature/ humidity sensor (DHT22)
 
 // DHT Settings
-#define DHTPIN D6         // what digital pin we're connected to. If you are not using NodeMCU change D6 to real pin
+#define DHTPIN D6         // what digital pin the DHT22 is connected to.
 #define DHTTYPE DHT22     // DHT 22  (AM2302), AM2321
 
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
 const boolean IS_METRIC = true;
-
 DHT dht(DHTPIN, DHTTYPE);
 
+
 void setup() {
+  // Enabling serialport (115.200 baud-rate)
   Serial.begin(115200);
   delay(10);
+
+  // Enabling display - set font to something small
+  u8x8.begin();
+  u8x8.setPowerSave(0);
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
 
   // We start by connecting to a WiFi network
 
@@ -75,6 +75,12 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println("--------");
+  u8x8.setCursor(0, 6);
+  u8x8.print("SSID:");
+  u8x8.print(ssid);
+  u8x8.setCursor(0, 7);
+  u8x8.print(WiFi.localIP());
+
 }
 
 
@@ -86,16 +92,25 @@ void loop() {
   Serial.println(temperature);
   Serial.print("DHT22 Humidity:");
   Serial.println(humidity);
+  u8x8.setCursor(0, 0);
+  u8x8.print("Temp:");
+  u8x8.print(temperature);
+  u8x8.setCursor(0, 1);
+  u8x8.print("Hum.:");
+  u8x8.print(humidity);
+
 
   // calculate temperature in Fahrenheit
   float temperaturef = (temperature * 1.8) + 32;
 
   // rough estimate on dewpoint calculation
   float dewpoint = temperature - ((100 - humidity) / 5);
+  u8x8.setCursor(0, 2);
+  u8x8.print("Dewp:");
+  u8x8.print(dewpoint);
   float dewpointf = (dewpoint * 1.8) + 32;
 
-
-  // Use WiFiClient class to create TCP connections to various hosts
+    // Use WiFiClient class to create TCP connections to various hosts
   WiFiClient client;
 
   if (client.connect(host_ts, 80) > 0)  {
@@ -140,33 +155,34 @@ void loop() {
     Serial.println(urldom + " sent to " + domoticz);
   }
 
+ 
+    // Connect to WU and drop temp + humidity to it (temperature must be in Fahrenheit)
+    // see: http://help.wunderground.com/knowledgebase/articles/865575-pws-upload-protocol
 
-  // Connect to WU and drop temp + humidity to it (temperature must be in Fahrenheit)
-  // see: http://help.wunderground.com/knowledgebase/articles/865575-pws-upload-protocol
+    if (client.connect(hostwu, 80) > 0)   {
+      String url2 = "/weatherstation/updateweatherstation.php?ID=";
+      url2 += WUID;
+      url2 += "&PASSWORD=";
+      url2 += WUPASS;
+      url2 += "&dateutc=now";
+      url2 += "&tempf=";
+      url2 += temperaturef;
+      url2 += "&humidity=";
+      url2 += humidity;
+      url2 += "&dewptf=";
+      url2 += dewpointf;
+      url2 += "&softwaretype=ESP8266&action=updateraw";
 
-  if (client.connect(hostwu, 80) > 0)   {
-    String url2 = "/weatherstation/updateweatherstation.php?ID=";
-    url2 += WUID;
-    url2 += "&PASSWORD=";
-    url2 += WUPASS;
-    url2 += "&dateutc=now";
-    url2 += "&tempf=";
-    url2 += temperaturef;
-    url2 += "&humidity=";
-    url2 += humidity;
-    url2 += "&dewptf=";
-    url2 += dewpointf;
-    url2 += "&softwaretype=ESP8266&action=updateraw";
-
-    // This will send the request to the server
-    client.print(String("GET ") + url2 + " HTTP/1.1\r\n" +
-                 "Host: " + hostwu + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    delay(100);
-    client.stop();
-    Serial.println("-----------");
-    Serial.println(url2 + " sent to " + hostwu);
-  }
+      // This will send the request to the server
+      client.print(String("GET ") + url2 + " HTTP/1.1\r\n" +
+                   "Host: " + hostwu + "\r\n" +
+                   "Connection: close\r\n\r\n");
+      delay(100);
+      client.stop();
+      Serial.println("-----------");
+      Serial.println(url2 + " sent to " + hostwu);
+    }
+ 
 
   // Go to deep-sleep. To enable this see https://www.losant.com/blog/making-the-esp8266-low-powered-with-deep-sleep
   // For NodeMCU connect D0 (GPIO16) to RST
@@ -174,7 +190,6 @@ void loop() {
   Serial.print("ESP8266 in sleep mode for ");
   Serial.print(UPDATE_INTERVAL_SECONDS);
   Serial.println(" seconds");
-
   ESP.deepSleep(UPDATE_INTERVAL_SECONDS * 1000000);
 
 
